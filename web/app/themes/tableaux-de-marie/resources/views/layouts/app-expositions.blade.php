ade68bbf51c39d67b77618be4256fba193e602fb
Fouxy68 <michaelfuchs68@gmail.com>
2024-12-08 12:18:53 +0100

    Amélioration de la page mes expos

--- web/app/themes/tableaux-de-marie/resources/views/layouts/app-expositions.blade.php
+++ web/app/themes/tableaux-de-marie/resources/views/layouts/app-expositions.blade.php
@@ -18,29 +18,32 @@
     <main id="main" class="main w-full py-32">
 
         <div class="max-w-screen-3xl px-8 mx-auto">
-            <div class=" grid gap-6 lg:gap-20 grid-cols-1 mb-16 sm:grid-cols-4">
+            <div class=" grid gap-6 lg:gap-16 grid-cols-1 mb-16 sm:grid-cols-4">
 
                 @while ($posts->have_posts())
                     @php $posts->the_post(); @endphp
-                    <div class="expo-item">
-                        <img src="{{ get_the_post_thumbnail_url(get_the_ID()) }}" alt="{{ the_title() }}"
-                            class="w-full h-auto pb-6">
+                    <div class="expo-item hover:bg-gray-100 p-2">
+                        <a href="{{ the_permalink() }}" class="flex flex-col justify-between h-full">
+                            <img src="{{ get_the_post_thumbnail_url(get_the_ID()) }}" alt="{{ the_title() }}"
+                                class="w-full h-auto pb-6">
+                            <div>
+                                @if (!empty(get_field('date_de_lexpo_start')) && !empty(get_field('date_de_lexpo_stop')))
+                                    <div class="date pb-6">
+                                        <span class="day font-sans text-xs block uppercase">Du
+                                            {{ get_field('date_de_lexpo_start') }} au
+                                            {{ get_field('date_de_lexpo_stop') }}</span>
+                                    </div>
+                                @endif
 
-                        @if (!empty(get_field('date_de_lexpo_start')) && !empty(get_field('date_de_lexpo_stop')))
-                            <div class="date pb-6">
-                                <span class="day font-sans text-xs block uppercase">Du
-                                    {{ get_field('date_de_lexpo_start') }} au
-                                    {{ get_field('date_de_lexpo_stop') }}</span>
+                                <span
+                                    class="stretched-link title text-lg xl:text-md font-title leading-normal pb-6">{{ the_title() }}</span>
+                                <div class="content">
+                                    <p>
+                                        {{ the_excerpt() }}
+                                    </p>
+                                </div>
                             </div>
-                        @endif
-
-                        <a href="{{ the_permalink() }}"
-                            class="stretched-link title text-lg xl:text-md font-title leading-normal pb-6">{{ the_title() }}</a>
-                        <div class="content">
-                            <p>
-                                {{ the_excerpt() }}
-                            </p>
-                        </div>
+                        </a>
                     </div>
                 @endwhile
                 @php wp_reset_postdata() @endphp
